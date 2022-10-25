8374d62741e7f959bd24e18b36f2f8bbffe946bc
Aaron <Aaron@DESKTOP-77A88FP.wifi.uc.edu>
2022-10-24 20:31:32 -0400

    Added Brownies and credential to main

--- Dori_Assignment09/MainPackage/Main.py
+++ Dori_Assignment09/MainPackage/Main.py
@@ -1,7 +1,14 @@
 '''
-Created on Oct 20, 2022
-
-@author: User
+Created on Oct 24, 2022
+Name: Amanda Rapien, Aaron Paulson, Ben Truax, Matthew Lamb
+email: Lambmj@mail.uc.edu, rapienaa@mail.uc.edu, paulsoar@mail.uc.edu,truaxbp@mail.uc.edu
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: This project demonstrates that we can develop an Eclipse project 
+modeling something in the real world, and collaborate on GitHub.
+Citations: N/A
+Anything else that's relevant:
 '''
 from DessertPackage.CupcakeClass import *
 from DessertPackage.SkittlesClass import *
@@ -36,3 +43,18 @@ print(skittles.__str__())
 #This gets the flavor 
 skittles.getFlavor()
 print(skittles.flavor)
+
+brownies = Brownies("Chocolate", 2)
+print(brownies.__str__())
+
+print(brownies.flavor) 
+
+#Negative Test
+brownies.setPrice(-3)
+#Positive Test
+brownies.setPrice(3)
+print(brownies.__str__())
+
+#This gets the flavor 
+brownies.getFlavor()
+print(brownies.flavor)
